8d15ea9736358e152d880bca4e6c076cb5b6044f
emiliobresolin <emiliotdm@hotmail.com>
2022-04-10 12:10:25 -0300

    comencando a implementar o tabuleiro

---
+++ ChessConsole/tabuleiro/Cor.cs
@@ -0,0 +1,18 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChessConsole.tabuleiro
+{
+    enum Cor
+    {
+        Branca,
+        Preta,
+        Vermelha,
+        Amarela,
+        Verde,
+        Laranja
+    }
+}
